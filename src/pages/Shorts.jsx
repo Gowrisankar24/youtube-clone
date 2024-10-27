9e5fdcd49a1c564d2210ed1cc5e98b392a2dad47
GowrisankarKumaresan <Gowrisankar.Kumaresan@lumen.com>
2024-10-27 11:39:33 +0530

    shorts responsive issue fixed

--- src/pages/Shorts.jsx
+++ src/pages/Shorts.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useEffect, useState } from 'react';
 import { collection, onSnapshot, query } from 'firebase/firestore';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { db } from '../Firebase';
 import { Skeleton } from '@mui/material';
 import { BiSolidDislike, BiSolidLike } from 'react-icons/bi';
@@ -9,10 +9,8 @@ import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 const Shorts = () => {
     const navigate = useNavigate();
     const { shortsId } = useParams();
-    const location = useLocation();
     const [shortsData, setShortsData] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [player, setPlayer] = useState(null);
 
     //get shorts Data
     useEffect(() => {
@@ -54,46 +52,51 @@ const Shorts = () => {
     }
     return (
         <>
-            <div className="flex justify-center h-screen relative overflow-hidden">
+            <div className="flex justify-center h-screen relative min-h-screen max-h-screen">
                 <div className="flex justify-center">
                     <div className="flex flex-row justify-center items-center max-w-full">
                         <iframe
                             width="30%"
                             height="100%"
-                            src={`https://www.youtube.com/embed/${currentShort.link}?rel=0&modestbranding=1&autoplay=1&loop=0&autohide=0&controls=0&nologo=0&iv_load_policy=3`}
+                            src={`https://www.youtube.com/embed/${currentShort?.link}?autoplay=1&loop=0&autohide=0&controls=0&nologo=0&iv_load_policy=3&rel=0&modestbranding=1`}
+                            title={currentShort?.name}
                             allow="autoplay; encrypted-media; accelerometer;picture-in-picture; web-share"
                             allowFullScreen
                             referrerPolicy="strict-origin-when-cross-origin"
-                            className="rounded-lg object-contain aspect-video max-w-full"
+                            className="rounded-lg object-contain aspect-video max-w-full min-h-full h-full max-h-full"
                             style={{ maxHeight: '90vh' }}
                         />
-                        <div className="flex flex-col relative left-5  top-10 gap-6 md:gap-3 xl:gap-2 2xl:gap-1">
+                        <div className="flex flex-col justify-end items-center text-center relative left-5 gap-2 h-[90vh] max-h-[90vh] min-[90vh]: md:gap-3 xl:gap-2 2xl:gap-1">
                             <div className="shorts-icons-wrapper">
-                                <span className="shorts-icons">
-                                    <BiSolidLike size={20} className="!text-yt-white" />
+                                <span className="shorts-icons text-size">
+                                    <BiSolidLike className="!text-yt-white" />
                                 </span>
-                                <span className="ms-1 !text-yt-white">{currentShort?.like}</span>
+                                <span className=" !text-yt-white items-center font-normal">
+                                    {currentShort?.like}
+                                </span>
                             </div>
                             <div className="shorts-icons-wrapper">
-                                <span className="shorts-icons">
-                                    <BiSolidDislike size={20} className="!text-yt-white" />
+                                <span className="shorts-icons  text-size">
+                                    <BiSolidDislike className="!text-yt-white " />
                                 </span>
-                                <span className="ms-1 !text-yt-white">{currentShort?.dislike}</span>
+                                <span className="!text-yt-white font-normal">
+                                    {currentShort?.dislike}
+                                </span>
                             </div>
-                            <div className="shorts-icons-wrapper">
-                                <span className="shorts-icons">
-                                    <MdOutlineComment size={20} className="!text-yt-white" />
+                            <div className="shorts-icons-wrapper ">
+                                <span className="shorts-icons text-size">
+                                    <MdOutlineComment className="!text-yt-white" />
                                 </span>
-                                <span className="ms-1 !text-yt-white">
+                                <span className="!text-yt-white font-normal">
                                     {currentShort?.comments}
                                 </span>
                             </div>
-                            <div className="shorts-icons-wrapper">
+                            <div className="shorts-icons-wrapper text-size">
                                 <img
                                     src={currentShort?.logo}
                                     alt={currentShort?.imgname}
-                                    className="rounded-full"
-                                    width={50}
+                                    className="rounded-full w-3 bg-cover sm:w-7 md:w-8 lg:w-14 xl:w-16 2xl:w-16"
+                                    // width={50}
                                 />
                             </div>
                         </div>
